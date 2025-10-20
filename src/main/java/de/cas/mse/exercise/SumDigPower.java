0842c19bb5aadfe03774886cc17120760de86bb0
Daniel S. <50729918+Ohrhusten@users.noreply.github.com>
2025-10-20 13:00:30 +0000

    Gruppe 4

--- src/main/java/de/cas/mse/exercise/SumDigPower.java
+++ src/main/java/de/cas/mse/exercise/SumDigPower.java
@@ -2,40 +2,40 @@ package de.cas.mse.exercise;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.LongStream;
 
 public class SumDigPower {
 
-	public List<Long> sumDigPow(long a, long b) {
+	public List<Long> sumDigPow(long lowerInclusive, long upperInclusive) {
+		List<Long> eurekaNumbers = new ArrayList<Long>();
 
-		List<Long> result = new ArrayList<Long>();
-
-		for (long i = a; i < b; i++) {
-
-			List<Long> longs = new ArrayList<Long>();
-
-			String temp = Long.toString(i);
-
-			// Split values
-			for (int j = 0; j < temp.length(); j++) {
-				longs.add(Long.valueOf(temp.substring(j, j + 1)));
+		// Kein Toter Code nur ne andere lösung ;-)
+		//var result = List.of(LongStream.range(lowerInclusive, upperInclusive+1).filter(this::isEurekaNumber));
+		
+		for (long number = lowerInclusive; number <= upperInclusive; number++) {
+			if (isEurekaNumber(number)) {
+				eurekaNumbers.add(number);
 			}
-
-			// Create sum
-			long sum = 0;
-			for (int j = 1; j <= longs.size(); j++) {
-				sum += Math.pow(longs.get(j - 1), j);
-			}
-
-			// Test if sum is equal
-			if (sum == i) {
-				result.add(sum);
-			}
-
 		}
 
-        System.out.println(result);
-		return result;
+        System.out.println(eurekaNumbers);
+		return eurekaNumbers;
+	}
 
+	private boolean isEurekaNumber(long number) {
+		String digits = Long.toString(number);
+
+		long sum = 0;
+		for (int digitIndex = 0; digitIndex < digits.length(); digitIndex++) {
+			var digit = getDigitAtIndex(digits, digitIndex);
+			sum += Math.pow(digit, digitIndex + 1);
+		}
+
+		return sum == number;
+	}
+
+	private Long getDigitAtIndex(String number, int index) {
+		return Long.valueOf(number.substring(index, index+1));
 	}
 
 }
